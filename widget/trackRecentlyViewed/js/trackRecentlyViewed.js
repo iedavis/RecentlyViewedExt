fba9907899162a3e4ca0d9ec34e324edef9d9011
Ian Davis <ian@davis-uk.net>
2015-01-12 18:29:33 -0500

    Re-organised code
    Alterations so that the Tracker is only responsible for maintaining the
    cookie with ID and ViewDate.
    
    ViewRecentlyViewed now retrieves the cookie, pulls the product details
    via REST call, and keep fresh on Product View events.

--- widget/trackRecentlyViewed/js/trackRecentlyViewed.js
+++ widget/trackRecentlyViewed/js/trackRecentlyViewed.js
@@ -11,12 +11,12 @@ define(
   //-------------------------------------------------------------------
   // DEPENDENCIES
   //-------------------------------------------------------------------
-  ['knockout', 'pubsub', 'js/date'],
+  ['knockout', 'pubsub', 'ccRestClient', 'js/date'],
   
   //-------------------------------------------------------------------
   // MODULE DEFINITION
   //-------------------------------------------------------------------
-  function(ko, pubsub) {
+  function(ko, pubsub, ccRestClient) {
 
     "use strict";
 
@@ -24,28 +24,7 @@ define(
 
       onLoad: function(widgetModel) {
 
-        // Validate products in localStorage
-        var recentlyViewedArray = JSON.parse(localStorage.getItem("cc.product.recentlyViewed")) || [];
-
-        var cutoffDate = Number(widgetModel.lifespan()).days().ago();
-
-        var thresholdMarker = recentlyViewedArray.length;
-        for(var i = 0; i < recentlyViewedArray.length; i++){
-          var viewedDate = Date.parse(recentlyViewedArray[i].dateViewed);
-          if(viewedDate.compareTo(cutoffDate) === -1){
-              thresholdMarker = i;
-              break;
-          }
-        }
-
-        recentlyViewedArray.length = thresholdMarker;
-        localStorage["cc.product.recentlyViewed"] = JSON.stringify(recentlyViewedArray);
-
-        // Check all Prouct ID still exist in catalog
-
-
         $.Topic(pubsub.topicNames.PRODUCT_VIEWED).subscribe(this.trackProductViewed);
-
       },
 
       trackProductViewed: function(product) {
@@ -54,35 +33,26 @@ define(
         var today = Date.today();
 
         // Retrieve array from localStorage (or create an empty array if not in localStorage)
-        var recentlyViewedArray = JSON.parse(localStorage.getItem("cc.product.recentlyViewed")) || [];
+        var recentlyViewedCookie = JSON.parse(localStorage.getItem("cc.product.recentlyViewed")) || [];
 
         // See if the viewed product is already in the array, and remove it
         var foundIndex = -1;
-        for(var i = 0; i < recentlyViewedArray.length; i++){
-          if(recentlyViewedArray[i].id == product.id) foundIndex = i;
+        for(var i = 0; i < recentlyViewedCookie.length; i++){
+          if(recentlyViewedCookie[i].id == product.id) foundIndex = i;
         }
-        if (foundIndex > -1) recentlyViewedArray.splice(foundIndex, 1);
+        if (foundIndex > -1) recentlyViewedCookie.splice(foundIndex, 1);
 
         // Insert viewed product at top of array
-        recentlyViewedArray.unshift({
+        recentlyViewedCookie.unshift({
           "id": product.id,
           "dateViewed": today.getFullYear() + " " + (today.getMonth() + 1) + " " + today.getDate(),
-          "displayName": product.displayName,
-          "brand": product.brand,
-          "route": product.route,
-          "listPrice": product.listPrice(),
-          "salePrice": product.salePrice(),
-          "primaryLargeImageURL": product.primaryLargeImageURL,
-          "primaryMediumImageURL": product.primaryMediumImageURL,
-          "primarySmallImageURL": product.primarySmallImageURL,
-          "primaryThumbImageURL": product.primaryThumbImageURL
         });
 
         // Trim the array to the maximum length (+1 to cater for Product Details Page)
-        if(recentlyViewedArray.length > viewHistoryLength)recentlyViewedArray.length = viewHistoryLength + 1;
+        if(recentlyViewedCookie.length > viewHistoryLength)recentlyViewedCookie.length = viewHistoryLength + 1;
 
         // Place array back into localStorage
-        localStorage["cc.product.recentlyViewed"] = JSON.stringify(recentlyViewedArray);
+        localStorage["cc.product.recentlyViewed"] = JSON.stringify(recentlyViewedCookie);
       }
     };
   }
--- widget/viewRecentlyViewed/js/viewRecentlyViewed.js
+++ widget/viewRecentlyViewed/js/viewRecentlyViewed.js
@@ -12,33 +12,124 @@ define(
   //-------------------------------------------------------------------
   // DEPENDENCIES
   //-------------------------------------------------------------------
-  ['knockout'],
+  ['knockout', 'pubsub', 'ccRestClient'],
 
   //-------------------------------------------------------------------
   // MODULE DEFINITION
   //-------------------------------------------------------------------
-  function(ko) {
+  function(ko, pubsub, ccRestClient) {
 
     "use strict";
+
+    var self;
 
     return {
 
       onLoad: function(widget) {
+        self = widget;
         widget.recentProducts = ko.observableArray();
+        widget.recentProductsAll = [];
+
+        var recentlyViewedCookie = JSON.parse(localStorage.getItem("cc.product.recentlyViewed")) || [];
+
+//        var cutoffDate = Number(widgetModel.lifespan()).days().ago();
+//        var thresholdMarker = recentlyViewedArray.length;
+//        for(var i = 0; i < recentlyViewedArray.length; i++){
+//          var viewedDate = Date.parse(recentlyViewedArray[i].dateViewed);
+//          if(viewedDate.compareTo(cutoffDate) === -1){
+//              thresholdMarker = i;
+//              break;
+//          }
+//        }
+//        recentlyViewedArray.length = thresholdMarker;
+
+
+        // Refresh the Recently Viewed Data
+        if(recentlyViewedCookie.length > 0){
+            
+            var pProductIds = [];
+            for(var j = 0; j < recentlyViewedCookie.length; j++){
+              pProductIds.push(recentlyViewedCookie[j].id);
+            }
+    
+            var pData = {
+                catalogId: "",
+                productIds: pProductIds
+            };
+    
+            ccRestClient.request('listProducts', pData,
+              function (upToDateProductData) {
+                for(var k = 0; k < recentlyViewedCookie.length; k++){
+                    widget.recentProductsAll[k] = {};
+                    widget.recentProductsAll[k].id = upToDateProductData[k].id;
+                    widget.recentProductsAll[k].displayName = upToDateProductData[k].displayName;
+                    widget.recentProductsAll[k].brand = upToDateProductData[k].brand;
+                    widget.recentProductsAll[k].route = upToDateProductData[k].route;
+                    widget.recentProductsAll[k].listPrice = upToDateProductData[k].listPrice;
+                    widget.recentProductsAll[k].salePrice = upToDateProductData[k].salePrice;
+                    widget.recentProductsAll[k].primaryLargeImageURL = upToDateProductData[k].primaryLargeImageURL;
+                    widget.recentProductsAll[k].primaryMediumImageURL = upToDateProductData[k].primaryMediumImageURL;
+                    widget.recentProductsAll[k].primarySmallImageURL = upToDateProductData[k].primarySmallImageURL;
+                    widget.recentProductsAll[k].primaryThumbImageURL = upToDateProductData[k].primaryThumbImageURL;
+                }
+                self.refreshDisplay();
+                $.Topic(pubsub.topicNames.PRODUCT_VIEWED).subscribe(self.productViewed);
+              },
+              function (upToDateProductData) {
+                console.log("Failed to refresh Recently Viewed");
+              }
+            );
+            
+        } else {
+            $.Topic(pubsub.topicNames.PRODUCT_VIEWED).subscribe(self.productViewed);
+        }
+
+      },
+
+
+      productViewed: function(product) {
+
+        // See if the viewed product is already in the array, and remove it
+        var foundIndex = -1;
+        for(var i = 0; i < self.recentProductsAll.length; i++){
+          if(self.recentProductsAll[i].id == product.id) foundIndex = i;
+        }
+        if (foundIndex > -1) self.recentProductsAll.splice(foundIndex, 1);
+
+        var viewedProduct = {};
+        viewedProduct.id = product.id;
+        viewedProduct.displayName = product.displayName;
+        viewedProduct.brand = product.brand;
+        viewedProduct.route = product.route;
+        viewedProduct.listPrice = product.listPrice;
+        viewedProduct.salePrice = product.salePrice;
+        viewedProduct.primaryLargeImageURL = product.primaryLargeImageURL;
+        viewedProduct.primaryMediumImageURL = product.primaryMediumImageURL;
+        viewedProduct.primarySmallImageURL = product.primarySmallImageURL;
+        viewedProduct.primaryThumbImageURL = product.primaryThumbImageURL;
+
+        // Insert viewed product at top of array
+        self.recentProductsAll.unshift(viewedProduct);
+        self.refreshDisplay();
+
+      },
+
+      refreshDisplay: function() {
+        self.recentProducts.removeAll();
+        var numDisplayed = Math.min(self.numDisplayed() || 3 , self.recentProductsAll.length);
+
+        for(var j = 0; j < self.recentProductsAll.length; j++){
+            if (self.recentProductsAll[j].id !== self.pageContextId){
+                self.recentProducts.push(self.recentProductsAll[j]);
+            }
+            if (self.recentProducts().length === numDisplayed) break;
+        }
 
       },
 
       beforeAppear: function(page) {
-        self = this;
-        var prodArray = JSON.parse(localStorage.getItem("cc.product.recentlyViewed")) || [];
-        for(var j = 0; j < prodArray.length; j++){
-            if (page.contextId && prodArray[j].id == page.contextId) prodArray.splice(j, 1);
-        }
-        self.recentProducts.removeAll();
-        var numDisplayed = Math.min(self.numDisplayed() || 3 , prodArray.length);
-        for(var i = 0; i < numDisplayed; i++){
-            self.recentProducts.push(prodArray[i]);
-        }
+        self.pageContextId = page.contextId;
+        self.refreshDisplay();
       }
     };
   }
